7aa851b3317e517a1e4ec39b921af68352d6767a
Gayathri <gayathrialagiri2003@gmail.com>
2025-09-16 17:04:38 +0530

    Add project files.

---
+++ Myfirstproject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Animal
+{
+   public virtual void sound()
+   {
+      Console.WriteLine("Animal makes a sound");
+    }
+
+}
+class Dog : Animal
+{
+    public override void sound()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Animal a = new Dog();
+        a.sound();
+        Console.WriteLine("");
+    }
+}
+
