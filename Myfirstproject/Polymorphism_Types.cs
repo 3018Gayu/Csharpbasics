cbeef4ee8935b97bedfe807aba56e03839721d6d
Gayathri <gayathrialagiri2003@gmail.com>
2025-09-17 10:28:06 +0530

    Added main method

--- Myfirstproject/Polymorphism_Types.cs
+++ Myfirstproject/Polymorphism_Types.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Myfirstproject
 {
-    internal class Polymorphism_Types
+    class Program
     {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Polymorphism Types in C#");
+        }
     }
 }
