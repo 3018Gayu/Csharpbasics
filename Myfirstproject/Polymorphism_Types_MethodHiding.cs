50b21e3b17584c3ca42143aef5bc5da118f23c07
Gayathri <gayathrialagiri2003@gmail.com>
2025-09-17 12:48:30 +0530

    First commit

--- Myfirstproject/Polymorphism_Types_MethodHiding.cs
+++ Myfirstproject/Polymorphism_Types_MethodHiding.cs
@@ -1,13 +1,36 @@
 ﻿using System;
-
-
 namespace Myfirstproject
 {
+    class Company
+    {
+        public void Show()
+        {
+            Console.WriteLine("Company Show Method");
+        }
+        public virtual void Display()  // Method Overriding
+        {
+            Console.WriteLine("Company Display Method");
+        }
+    }
+    class Employee : Company
+    {
+        public new void Show()  // Method Hiding
+        {
+            Console.WriteLine("Employee Show Method");
+        }
+        public override void Display()  // Method Overriding
+        {
+            Console.WriteLine("Employee Display Method");
+        }
+    }
     class Program
     {
         static void Main(String[] args)
         {
             Console.WriteLine("Polymorphism Types in C#");
+            Company c=new Employee();
+            c.Show(); // Method Hiding
+            c.Display(); // Method Overriding
         }
     }
 }
